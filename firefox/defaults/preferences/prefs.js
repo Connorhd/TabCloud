132a885e9c0ea9fe94a3ebb24a444b034fc527e2
Connor Dunn <connorhd@gmail.com>
2011-01-19 17:56:03 +0000

    Remove unused preferences files in Firefox extension.

--- firefox/defaults/preferences/prefs.js
+++
@@ -1,6 +0,0 @@
-pref("extensions.tabcloud.boolpref", false);
-pref("extensions.tabcloud.intpref", 0);
-pref("extensions.tabcloud.stringpref", "A string");
-
-// https://developer.mozilla.org/en/Localizing_extension_descriptions
-//pref("extensions.tabcloud@firefox.connorhd.co.uk.description", "chrome://tabcloud/locale/overlay.properties");
